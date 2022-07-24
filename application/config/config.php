435b2d59888db4b7ef67fea9eb2018d3f42e5ed4
DESKTOP-I21H4UV\dell <dipen2016p@gmail.com>
2022-07-24 17:27:58 +0530

    changing base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = ($_SERVER['HTTP_HOST']=='localhost:3308' || $_SERVER['HTTP_HOST']=='localhost') ? 'http://localhost/demo_code/' : '';
+$config['base_url'] = ($_SERVER['HTTP_HOST']=='localhost:3308' || $_SERVER['HTTP_HOST']=='localhost') ? 'http://localhost/Url-Previe-Demo/' : '';
 
 /*
 |--------------------------------------------------------------------------
